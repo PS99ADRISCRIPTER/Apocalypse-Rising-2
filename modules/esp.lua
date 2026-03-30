c4f2729cd11915fde73333842d6d464438a194e9
Adrighg-scripts-TEST <nadineadrian2012.nb@yahoo.com>
2026-03-31 00:55:37 +0200

    Simplify highlight instance creation in ESP
    Refactor ESP toggle function to simplify highlight creation.

--- modules/esp.lua
+++ modules/esp.lua
@@ -3,17 +3,12 @@ local ESP = {}
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local enabled = false
-
 function ESP:Toggle(v)
-    enabled = v
-    
     for _, p in pairs(Players:GetPlayers()) do
         if p ~= LocalPlayer and p.Character then
             if v then
                 if not p.Character:FindFirstChild("Highlight") then
-                    local h = Instance.new("Highlight")
-                    h.Parent = p.Character
+                    Instance.new("Highlight", p.Character)
                 end
             else
                 for _, obj in pairs(p.Character:GetChildren()) do
