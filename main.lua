544edff7125b1c8dc3fea811d9e2602ab3a8bbad
Adrighg-scripts-TEST <nadineadrian2012.nb@yahoo.com>
2026-03-31 00:56:06 +0200

    Add Vehicle module and update UI initialization

--- main.lua
+++ main.lua
@@ -2,11 +2,12 @@ local base = "https://raw.githubusercontent.com/DEINNAME/Apocalypse-Rising-2/mai
 
 local Aim = loadstring(game:HttpGet(base.."modules/aim.lua"))()
 local ESP = loadstring(game:HttpGet(base.."modules/esp.lua"))()
+local Vehicle = loadstring(game:HttpGet(base.."modules/vehicle.lua"))()
 local Movement = loadstring(game:HttpGet(base.."modules/movement.lua"))()
 local XRay = loadstring(game:HttpGet(base.."modules/xray.lua"))()
 
 local UI = loadstring(game:HttpGet(base.."ui/window.lua"))()
 
-UI:Create(Aim, ESP, Movement, XRay)
+UI:Init(Aim, ESP, Vehicle, Movement, XRay)
 
-print("FULL SCRIPT LOADED")
+print("Full modular script loaded")
