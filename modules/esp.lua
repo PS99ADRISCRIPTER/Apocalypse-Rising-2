f56142b989cafc8a6e16ae3c97fc280a29bf1ed5
Adrighg-scripts-TEST <nadineadrian2012.nb@yahoo.com>
2026-03-31 00:46:49 +0200

    Add ESP module for player highlighting
    Implement ESP module to toggle player highlights.

---
+++ modules/esp.lua
@@ -0,0 +1,27 @@
+local ESP = {}
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local enabled = false
+
+function ESP:Toggle(v)
+    enabled = v
+    
+    for _, p in pairs(Players:GetPlayers()) do
+        if p ~= LocalPlayer and p.Character then
+            if v then
+                local h = Instance.new("Highlight")
+                h.Parent = p.Character
+            else
+                for _, obj in pairs(p.Character:GetChildren()) do
+                    if obj:IsA("Highlight") then
+                        obj:Destroy()
+                    end
+                end
+            end
+        end
+    end
+end
+
+return ESP
