740c25d0bdcb8da41f9cfe7e1c4071b2cd8c5d0d
Adrighg-scripts-TEST <nadineadrian2012.nb@yahoo.com>
2026-03-31 00:58:23 +0200

    Enhance UI with additional features and tabs

--- ui/window.lua
+++ ui/window.lua
@@ -2,30 +2,46 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local UI = {}
 
-function UI:Create(Aim, ESP)
+function UI:Init(Aim, ESP, Vehicle, Movement, XRay)
 
     local Window = Rayfield:CreateWindow({
-        Name = "My Script",
-        LoadingTitle = "Loading...",
-        LoadingSubtitle = "by you"
+        Name = "Apoc Script",
+        LoadingTitle = "Loading",
+        LoadingSubtitle = "..."
     })
 
-    local tab = Window:CreateTab("Main")
+    local main = Window:CreateTab("Main")
+    local visuals = Window:CreateTab("Visuals")
+    local player = Window:CreateTab("Player")
 
-    tab:CreateToggle({
-        Name = "Aim Assist",
-        CurrentValue = false,
-        Callback = function(v)
-            Aim:Toggle(v)
-        end
+    main:CreateToggle({
+        Name = "Aim",
+        Callback = function(v) Aim:Toggle(v) end
     })
 
-    tab:CreateToggle({
-        Name = "ESP",
-        CurrentValue = false,
-        Callback = function(v)
-            ESP:Toggle(v)
-        end
+    visuals:CreateToggle({
+        Name = "Player ESP",
+        Callback = function(v) ESP:Toggle(v) end
+    })
+
+    visuals:CreateToggle({
+        Name = "Vehicle ESP",
+        Callback = function(v) Vehicle:Toggle(v) end
+    })
+
+    player:CreateToggle({
+        Name = "Speed",
+        Callback = function(v) Movement:Speed(v) end
+    })
+
+    player:CreateToggle({
+        Name = "Infinite Jump",
+        Callback = function(v) Movement:Jump(v) end
+    })
+
+    player:CreateButton({
+        Name = "XRay",
+        Callback = function() XRay:Toggle() end
     })
 
 end
